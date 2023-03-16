a402dad4e17ed5a023d14c77b220d3030b181a20
Fadin Vladislav <sad_456@ngs.ru>
2023-03-17 02:13:30 +0700

    Update ValidateYear.kt

--- app/src/main/java/com/example/surfit/domain/useCase/ValidateYear.kt
+++ app/src/main/java/com/example/surfit/domain/useCase/ValidateYear.kt
@@ -8,6 +8,9 @@ class ValidateYear {
             if (year.isEmpty()) {
                 return ValidationResult(false, "Поле не может быть пустым")
             }
+            if(year.length != 4) {
+                return ValidationResult(false, "Год должен быть четырехзначным числом")
+            }
             year.toInt()
         } catch (e: Exception) {
             return ValidationResult(false, "Поле может содержать только цифры")
